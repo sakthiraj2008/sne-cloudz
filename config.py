6a8f27e3cf47275a48ea2dbda4f8f34f699bd056
TN Botz <163137175+sakthiraj2008@users.noreply.github.com>
2025-08-30 08:33:49 +0530

    Update and rename config.py.txt to config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+BOT_TOKEN = "8377654500:AAGilXL4wKiruj3SdsJduxxUHr7jJ7L9t1k"
+CHANNEL_ID = "@sne_cloud_channel"
+MONGO_URI = "mongodb+srv://sakthirajbaskaran:gqJb3gPWMc4GqhYB@ani-tubez.ctim8nc.mongodb.net/?retryWrites=true&w=majority"
+
+UPLOAD_FOLDER = "uploads"
